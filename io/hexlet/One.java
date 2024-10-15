2ef3c05eed36da05dd53300a4d0bb0f0ed6e98fa
nika7407 <nikala7407@gmail.com>
2024-10-15 22:34:30 +0400

    i hate compilation

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,14 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        // Переворачиваем слово "Hexlet"
+        String word = "Hexlet";
+        String reversedWord = StringUtils.reverse(word);
+        
+        // Выводим результат
+        System.out.println(reversedWord);  // Ожидаемый вывод: telxeH
     }
 }
-
